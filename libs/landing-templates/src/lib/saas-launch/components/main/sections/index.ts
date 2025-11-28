89c558aebc70a0c6483fffefbe5a1271be3ea993
Ayellet Tal <tal.ayellet@gmail.com>
2025-11-28 17:51:25 +0200

    fix: Renames section files

--- libs/landing-templates/src/lib/saas-launch/components/main/sections/index.ts
+++ libs/landing-templates/src/lib/saas-launch/components/main/sections/index.ts
@@ -2,5 +2,5 @@ export * from './features';
 export * from './final-cta';
 export * from './hero';
 export * from './stats';
-export * from './about-section';
-export * from './contact-section';
+export * from './about';
+export * from './contact';
